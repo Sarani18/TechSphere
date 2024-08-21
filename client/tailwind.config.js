52eacdb846f363362bace173e75e999871a2b05b
Sarani18 <sarani.stardust@gmail.com>
2024-08-21 06:41:50 +0530

    Header refined

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,11 +6,13 @@ export default {
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('flowbite/plugin'),
+    flowbite.plugin(),
   ],
-}
+};
